493fa707f46f2a345ccda50c7c1b927da3120a0d
vegaminer <44702038+vegaminer@users.noreply.github.com>
2019-11-26 16:07:10 +0300

    Redmine 4.0.4, Rails 5.2.3

--- lib/redmine_issue_sla/infectors/projects_helper.rb
+++ lib/redmine_issue_sla/infectors/projects_helper.rb
@@ -3,9 +3,9 @@ module RedmineIssueSla
     module ProjectsHelper
       module ClassMethods; end
 
-      module InstanceMethods
-        def project_settings_tabs_with_issue_sla
-          tabs = project_settings_tabs_without_issue_sla
+      #module InstanceMethods
+        def project_settings_tabs
+          tabs = super
           return tabs unless @project.module_enabled?('redmine_issue_sla')
 
           if User.current.allowed_to?(:manage_issue_sla, @project)
@@ -14,7 +14,7 @@ module RedmineIssueSla
           tabs
         end
 
-        def _retrieve_slas(project)
+        def retrieve_slas(project)
           slas = project.issue_slas
           ::IssuePriority.all.each do |p|
             next if slas.any? {|s| s.priority_id == p.id }
@@ -22,17 +22,19 @@ module RedmineIssueSla
           end
           project.issue_slas.reload
         end
-      end
+      #end
 
-      def self.included(receiver)
-        receiver.extend         ClassMethods
-        receiver.send :include, InstanceMethods
-        receiver.class_eval do
-          unloadable
-          alias_method_chain :project_settings_tabs, :issue_sla
-          alias_method :retrieve_slas, :_retrieve_slas
-        end
-      end
+      #def self.included(receiver)
+      #  receiver.extend         ClassMethods
+      #  receiver.send :include, InstanceMethods
+      #  receiver.class_eval do
+      #    unloadable
+      #    alias_method_chain :project_settings_tabs, :issue_sla
+      #    alias_method :retrieve_slas, :_retrieve_slas
+      #  end
+      #end
     end
   end
 end
+
+ProjectsHelper.prepend RedmineIssueSla::Infectors::ProjectsHelper
