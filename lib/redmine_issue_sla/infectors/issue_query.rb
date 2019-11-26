9ded52275ce780620c3765250f68fb517c82152c
vegaminer <44702038+vegaminer@users.noreply.github.com>
2019-11-26 17:13:45 +0300

    Elimintaing alias_method_chain

--- lib/redmine_issue_sla/infectors/issue_query.rb
+++ lib/redmine_issue_sla/infectors/issue_query.rb
@@ -3,11 +3,11 @@ module RedmineIssueSla
     module IssueQuery
       module ClassMethods; end
 
-      #module InstanceMethods
+      module InstanceMethods
 
-          def available_filters
+          def available_filters_with_issue_sla
             return @available_filters if @available_filters
-            super
+            available_filters_without_issue_sla
 
             if User.current.allowed_to?(:view_issue_sla, project, :global => true)
               field = "expiration_date"
@@ -22,9 +22,9 @@ module RedmineIssueSla
             @available_filters
           end
 
-          def available_columns
+          def available_columns_with_issue_sla
             return @available_columns if @available_columns
-            super
+            available_columns_without_issue_sla
 
             if User.current.allowed_to?(:view_issue_sla, project, :global => true)
               @available_columns.push QueryColumn.new(:expiration_date,
@@ -36,21 +36,26 @@ module RedmineIssueSla
             @available_columns
           end
 
-      #end
+      end
 
-      #def self.included(receiver)
-      #  receiver.extend(ClassMethods)
-      #  receiver.send(:include, InstanceMethods)
-      #  receiver.class_eval do
-      #    unloadable
+      def self.included(receiver)
+        receiver.extend(ClassMethods)
+        receiver.send(:include, InstanceMethods)
+        receiver.class_eval do
+          unloadable
 
-      #    alias_method_chain :available_filters, :issue_sla
-      #    alias_method_chain :available_columns, :issue_sla
-      #  end
-      #end
+          #alias_method_chain :available_filters, :issue_sla
+          #alias_method_chain :available_columns, :issue_sla
+          alias_method :available_filters_without_issue_sla, :available_filters
+          alias_method :available_filters, :available_filters_with_issue_sla
+          
+          alias_method :available_columns_without_issue_sla, :available_columns
+          alias_method :available_columns, :available_columns_with_issue_sla          
+        end
+      end
 
     end
   end
 end
 
-Issue.prepend RedmineIssueSla::Infectors::IssueQuery
+#Issue.prepend RedmineIssueSla::Infectors::IssueQuery
