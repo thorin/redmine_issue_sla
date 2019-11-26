9d47c22181c0916065f369ca51879379e80a9ea4
vegaminer <44702038+vegaminer@users.noreply.github.com>
2019-11-26 17:07:52 +0300

    Update projects_helper.rb

--- lib/redmine_issue_sla/infectors/projects_helper.rb
+++ lib/redmine_issue_sla/infectors/projects_helper.rb
@@ -4,9 +4,9 @@ module RedmineIssueSla
       module ClassMethods; end
 
       module InstanceMethods
-        def project_settings_tabs
-          tabs = super
-          #return tabs unless @project.module_enabled?('redmine_issue_sla')
+        def project_settings_tabs_with_issue_sla
+          tabs = project_settings_tabs_without_issue_sla
+          return tabs unless @project.module_enabled?('redmine_issue_sla')
 
           if User.current.allowed_to?(:manage_issue_sla, @project)
             tabs << {:name => 'issue_sla', :action  => :manage_issue_sla, :partial => 'projects/settings/issue_sla', :label => :label_issue_sla}
@@ -14,7 +14,7 @@ module RedmineIssueSla
           tabs
         end
 
-        def retrieve_slas(project)
+        def _retrieve_slas(project)
           slas = project.issue_slas
           ::IssuePriority.all.each do |p|
             next if slas.any? {|s| s.priority_id == p.id }
@@ -24,17 +24,19 @@ module RedmineIssueSla
         end
       end
 
-      #def self.included(receiver)
-      #  receiver.extend         ClassMethods
-      #  receiver.send :include, InstanceMethods
-      #  receiver.class_eval do
-      #    unloadable
-      #    alias_method_chain :project_settings_tabs, :issue_sla
-      #    alias_method :retrieve_slas, :_retrieve_slas
-      #  end
-      #end
+      def self.included(receiver)
+        receiver.extend         ClassMethods
+        receiver.send :include, InstanceMethods
+        receiver.class_eval do
+          unloadable
+          #alias_method_chain :project_settings_tabs, :issue_sla
+          alias_method :project_settings_tabs_without_issue_sla, :project_settings_tabs
+          alias_method :project_settings_tabs, :project_settings_tabs_with_issue_sla          
+          alias_method :retrieve_slas, :_retrieve_slas
+        end
+      end
     end
   end
 end
 
-ProjectsHelper.prepend RedmineIssueSla::Infectors::ProjectsHelper
+#ProjectsHelper.prepend RedmineIssueSla::Infectors::ProjectsHelper
